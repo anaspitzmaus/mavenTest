6e6d42bf3fdc2f84942a1da222922427e0fc91ef
user2 <user2@CLIENT2.KARDIO.local>
2020-12-30 11:07:01 +0100

    test

---
+++ maven_test/src/main/java/com/rose/maven_test/App.java
@@ -0,0 +1,17 @@
+package com.rose.maven_test;
+
+import javax.swing.JDialog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        JDialog myD = new JDialog();
+        myD.setVisible(true);
+        System.out.println( "Hello World!" );
+    }
+}
